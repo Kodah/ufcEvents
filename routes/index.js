5ca39440c414eea6dfca317466aa045564d2f997
Kodah <tom_1440@hotmail.co.uk>
2015-08-31 01:46:44 +0100

    Added parsing of json to modify the _id key Added drop database method

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var request = require("request");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -15,13 +15,13 @@ router.get('/userlist', function (req, res) {
 	collection.find({},{},function(e, docs){
         console.log(docs[1].username);
         res.render('userlist', {
-           "userlist" : docs
-       });
+         "userlist" : docs
+     });
     });
 });
 
 // GET Ufc data and put it in database
-router.post('/addUFCData', function(req, res){
+router.post('/addUFCData', function (req, res){
     var db = req.ufcdb;
 
     request({
@@ -31,17 +31,40 @@ router.post('/addUFCData', function(req, res){
 
         if (!error && response.statusCode === 200) {
 
-            var arrayOfEvents = JSON.parse(body);
-            arrayOfEvents.forEach(function(ufcevent)
-            { 
-                console.log(ufcevent); 
+            var doc = [];
+
+            body.forEach(function (ufcEvent, index) {
+
+                var ufcEventDictionary = {
+                    "_id": ufcEvent.id,
+                    "event_date": new Date(ufcEvent.event_date),
+                    "end_event_dategmt": new Date(ufcEvent.end_event_dategmt),
+                    "base_title": ufcEvent.base_title,
+                    "title_tag_line" : ufcEvent.title_tag_line,
+                    "feature_image" : ufcEvent.feature_image,
+                    "event_status" : ufcEvent.event_status,
+                    "arena" : ufcEvent.arena
+                };
+
+                doc.push(ufcEventDictionary);
+            });
+
+            doc.sort(function (a, b) {
+                return a.event_date > b.event_date ? 1 : 0; 
             });
 
             var collection = db.get('ufcEvents');
-            collection.insert(body);
+            collection.insert(doc);
             res.redirect("ufc");
         }
     });
+});
+
+router.post('/deleteUFCData', function (req, res) {
+    var db = req.ufcdb;
+    var collection = db.get('ufcEvents');
+    collection.drop();
+    res.redirect('ufc');
 });
 
 
@@ -57,12 +80,12 @@ router.get('/ufc', function (req, res) {
     });
 });
 
-router.get('/newuser', function(req, res){
+router.get('/newuser', function (req, res){
 	res.render('newuser', {title :  'Add New User'});
 });
 
 
-router.post('/adduser', function(req, res) {
+router.post('/adduser', function (req, res) {
 
     // Set our internal DB variable
     var db = req.db;
