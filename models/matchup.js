02de1a9682f5c12a0aad30ddb329c2de82621596
Tom Sugarev <Tom.Sugarev@3squared.com>
2015-09-07 17:36:35 +0100

    Add schema for matchup

--- models/matchup.js
+++ models/matchup.js
@@ -3,6 +3,24 @@ var Schema = mongoose.Schema;
 
 var matchupSchema = new Schema({
     _id: { type: String, required: true, unique: true },
+    event_id: String,
+    fight_description: String,
+
+	fighter1_id: String,
+    fighter1_first_name: String,
+    fighter1_last_name: String,
+    fighter1_nickname: String,
+    fighter1_weight_class:String
+	fighter1_full_body_image, String,
+    fighter1record: String,
+   
+    fighter2_id: String,
+    fighter2_first_name: String,
+    fighter2_last_name: String,
+    fighter2_nickname: String,
+    fighter2_weight_class:String
+	fighter2_full_body_image, String,
+    fighter2record: String,
 
     
 
